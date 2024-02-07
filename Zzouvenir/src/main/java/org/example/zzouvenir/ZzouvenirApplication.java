d7638c09ec3a922ba907a844594d4cc893b27d94
xethail <bertrandleger5@gmail.com>
2024-02-07 10:46:13 +0100

    first commit

---
+++ Zzouvenir/src/main/java/org/example/zzouvenir/ZzouvenirApplication.java
@@ -0,0 +1,13 @@
+package org.example.zzouvenir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZzouvenirApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZzouvenirApplication.class, args);
+    }
+
+}
---
+++ Zzouvenir/src/test/java/org/example/zzouvenir/ZzouvenirApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.zzouvenir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZzouvenirApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
