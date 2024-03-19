55fdce17bb91e7dd073c10520988ded9b7abf617
xethail <bertrandleger5@gmail.com>
2024-03-19 20:53:19 +0100

    first docker compose

--- Zzouvenir/src/main/java/org/zzouvenir/ArticleController.java
+++ Zzouvenir/src/main/java/org/zzouvenir/ArticleController.java
@@ -17,7 +17,7 @@ import org.slf4j.LoggerFactory;
 
 @RestController
 @RequestMapping("/api/articles")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://frontend")
 public class ArticleController {
 
     private static final Logger logger = LoggerFactory.getLogger(ArticleController.class);
